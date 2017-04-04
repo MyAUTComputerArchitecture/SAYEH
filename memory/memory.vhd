f7b4845b58032f6f3705a7e0ee18034d01d95280
anvari1313 <ahmad.anv@gmail.com>
2017-04-04 20:40:48 +0430

    Setting the size of memory.

--- memory/memory.vhd
+++ memory/memory.vhd
@@ -9,10 +9,16 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity MEMORY is
-	port (address : in std_logic_vector;
-		data_in : in std_logic_vector;
-		data_out : out std_logic_vector;
-		clk, rwbar : in std_logic);
+	generic(
+		MEMORY_WIDTH_SIZE 	: integer;		-- Size of each memory cell
+		MEMORY_ADDRESS_SIZE	: integer		-- Size of the momory can be obtained from it
+	       );
+	port(
+		address : in std_logic_vector(MEMORY_ADDRESS_SIZE - 1 downto 0);
+		data_in : in std_logic_vector(2 ** MEMORY_ADDRESS_SIZE - 1 downto 0) ;
+		data_out : out std_logic_vector(2 ** MEMORY_ADDRESS_SIZE - 1 downto 0);
+		clk, rwbar : in std_logic
+		);
 end entity MEMORY;
 
 architecture MEMORY_ARCH of MEMORY is
