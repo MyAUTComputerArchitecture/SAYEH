083ba2dba493f3bec164eede6a45f9d5dd2945ee
anvari1313 <ahmad.anv@gmail.com>
2017-04-06 13:16:19 +0430

    Implementing AND component

--- alu_components/and_component.vhd
+++ alu_components/and_component.vhd
@@ -2,11 +2,22 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity AND_COMPONENT is
-	generic();
-	port();
+	generic(
+		COMPONENT_SIZE	: integer
+	       );
+	port(
+		INPUT1		: in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+		INPUT2		: in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+		OUTPUT		: out std_logic_vector(COMPONENT_SIZE - 1 downto 0)
+	    );
 end component;
 
 architecture AND_COMPONENT_ARCH of AND_COMPONENT is
-begin
 
+begin
+	GATE_GEN:
+	for I in OUTPUT'range generate
+	begin
+		OUTPUT(i) <= INPUT1 and INPUT2;
+	end for;
 end architecture;
