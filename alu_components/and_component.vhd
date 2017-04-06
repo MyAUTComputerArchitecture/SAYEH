29962a1ffa8bb8df7662547ba95ee9abcb4cdd28
anvari1313 <ahmad.anv@gmail.com>
2017-04-06 12:30:54 +0430

    Adding and component to the ALU components

---
+++ alu_components/and_component.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity AND_COMPONENT is
+	generic();
+	port();
+end component;
+
+architecture AND_COMPONENT_ARCH of AND_COMPONENT is
+begin
+
+end architecture;
