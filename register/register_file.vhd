45de780afdf0796320c4fb1f1f07417795e09788
anvari1313 <ahmad.anv@gmail.com>
2017-04-08 17:30:29 +0430

    Implementing register file

--- register/register_file.vhd
+++ register/register_file.vhd
@@ -11,54 +11,62 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL;
 
--- This module is a register file containing REGISTER_COUNT regisers that each
--- register has REGESTER_SIZE bits and can be accessed to each register with SEL
--- signal.
-
+-- Caution: This module is not generic for the exclusive usage of this module in this project.
+-- Be careful about the parameters in the generic part and their meaning!!!!!!!!!!!!!!!!!!!!
 	
 entity REGISTER_FILE is
 	generic(
-		REGESTER_SIZE 	: integer;							-- Size of each register in the register file
-		REGISTER_COUNT 	: integer							-- Number of registers in the register file
+		REGESTER_SIZE 	: integer := 16;											-- Size of each register in the register file
+		ADDRESS_SIZE 	: integer := 6												-- Size of the address that can lead to the number of registers
 		);
 	
 	port(
 		IDATA		: in std_logic_vector(REGESTER_SIZE - 1 downto 0);						--	STD logic vector for input data
 		CLK			: in std_logic;															--	Clock input
-		SEL			: in std_logic_vector(4 - 1 downto 0);									--	Select signal for addressing to registers
-		RES 		: in std_logic;															--	Reset signal
-		WRITE_EN	: in std_logic;															--	Wrinting enable signal
-		READ_EN		: in std_logic;															--	Reading enable signal
-		ODATA		: out std_logic_vector(REGESTER_SIZE - 1 downto 0)						--	Output of data
+		WP_IN		: in std_logic_vector(ADDRESS_SIZE - 1 downto 0);						--	Select signal for addressing to registers
+		RF_L_WRITE	: in std_logic;															--	Write enable signal for low significant bit
+		RF_H_WRITE	: in std_logic;															--	Write enable signal for high significant bit
+		SELECTOR	: in std_logic_vector(3 downto 0);
+		LEFT_OUT	: out std_logic_vector(REGESTER_SIZE - 1 downto 0);						--	Output number 1
+		RIGHT_OUT	: out std_logic_vector(REGESTER_SIZE - 1 downto 0)						--	Output number 2
 		);
 end entity;
 
 architecture REGISTER_FILE_ARCH of REGISTER_FILE is
 	
-	type REGISTERS_TYPE is array(REGISTER_COUNT - 1 downto 0) of std_logic_vector(REGESTER_SIZE - 1 downto 0);
-	signal REGISTERS : REGISTERS_TYPE;
+	type REGISTER_FILE_TYPE is array (natural range <>, natural range <>) of std_logic;
+
 	
 begin	
 	
 	process(clk)
-			variable address : integer;
+		constant REGISTER_NUM : integer := 2 ** ADDRESS_SIZE;
+		variable REGISTER_FILE_ARR : REGISTER_FILE_TYPE(0 to REGISTER_NUM - 1,IDATA'range);
+		variable address : integer;
+		
 	begin
 		if clk'event and clk = '1' then
-			if(WRITE_EN = '1') then
-				address := to_integer(unsigned(SEL));
-				REGISTERS(address) <= IDATA;
-			end if;
-			if(READ_EN = '1') then
-				address := to_integer(unsigned(SEL));
-				ODATA <= REGISTERS(address);
-			end if;
-			if(RES = '1') then
-				for I in REGISTERS'low to REGISTERS'high loop
-					for J in REGISTERS(I)'low to REGISTERS(I)'high loop
-						REGISTERS(I)(J) <= '0';
-					end loop;
+			address := to_integer(unsigned(WP_IN));
+			if(RF_L_WRITE = '1') then
+				for I in  IDATA'length / 2 - 1 downto 0 loop
+					REGISTER_FILE_ARR(address,i) := IDATA(I);
 				end loop;
 			end if;
-		end if;
+			if(RF_H_WRITE = '1') then
+				for I in IDATA'length - 1 downto IDATA'length / 2 loop
+					REGISTER_FILE_ARR(address,i) := IDATA(I);
+				end loop;
+				
+			end if;
+
+			for I in RIGHT_OUT'range loop
+				RIGHT_OUT(I)	<= REGISTER_FILE_ARR(address + to_integer(unsigned(SELECTOR(1 downto 0))), I);
+			end loop;
+			
+			for I in LEFT_OUT'range loop
+				LEFT_OUT(I)		<= REGISTER_FILE_ARR(address + to_integer(unsigned(SELECTOR(3 downto 2))), I);
+			end loop;
+		
+			end if;
 	end process;
 end architecture;
