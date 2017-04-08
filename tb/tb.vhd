63cef97ab1f5faf2d2faf9d6cc300ec68ae49d4a
anvari1313 <ahmad.anv@gmail.com>
2017-04-08 15:05:23 +0430

    Changing test bench module

--- tb/tb.vhd
+++ tb/tb.vhd
@@ -6,43 +6,34 @@ entity TB is
 end TB;
 
 architecture TB_ARCH of TB is
-	component ha
+	component SHIFT_R_COMPONENT is
+		generic(
+			COMPONENT_SIZE	: integer
+		       );
 		port(
-			a:	in	std_logic;
-			b:	in	std_logic;
-			o:	out	std_logic;
-			c:	out	std_logic
-		);
+			INPUT		: in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+			OUTPUT		: out std_logic_vector(COMPONENT_SIZE - 1 downto 0)
+		    );
 	end component;
-	signal a, b, o, c : std_logic;
+	
+	signal a, o	: std_logic_vector(15 downto 0);
 begin
-	half_adder: ha port map(a => a, b => b, o => o, c => c);
+	MODULE: SHIFT_R_COMPONENT
+		generic map(16)
+       	port map( a ,o);
 	
 	process
 	begin
-		a <= 'X';
-		b <= 'X';
-
-		wait for 1 ns;
-
-		a <= '0';
-		b <= '0';
-
-		wait for 1 ns;
-
-		a <= '1';
-		b <= '0';
+		a <= "0000000000000001";
 		
 		wait for 1 ns;
 		
-		a <= '0';
-		b <= '1';
-
-		wait for 1 ns;
-
-		a <= '1';
-		b <= '1';
+		a <= "0111000010101010";
 		
+		wait for 1 ns;
+		
+		a <= "1111111111111111";
+
 		wait for 1 ns;
 
 		assert false report "Reached end of test";
