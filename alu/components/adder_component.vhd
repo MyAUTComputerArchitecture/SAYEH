296a284659376e20c1c03ddd09c189612f733227
anvari1313 <ahmad.anv@gmail.com>
2017-04-08 14:57:42 +0430

    Bug fix adder component

--- alu/components/adder_component.vhd
+++ alu/components/adder_component.vhd
@@ -11,21 +11,21 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ADDER_COMPONENT is
-	generic(
-		COMPONENT_SIZE	: integer := 2
-	       );
-	port(
-		INPUT1		: in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
-		INPUT2		: in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
-		SUM		: out std_logic_vector(COMPONENT_SIZE - 1 downto 0);
-		CARRY		: out std_logic
-	    );
+  generic(
+    COMPONENT_SIZE  : integer
+         );
+  port(
+    INPUT1    : in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+    INPUT2    : in  std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+    SUM    : out std_logic_vector(COMPONENT_SIZE - 1 downto 0);
+    CARRY    : out std_logic
+      );
 end entity;
 
 architecture ADDER_COMPONENT_ARCH of ADDER_COMPONENT is
-	signal RES		: std_logic_vector(COMPONENT_SIZE downto 0);
+  signal RES    : std_logic_vector(COMPONENT_SIZE downto 0);
 begin
-	RES <= std_logic_vector(to_integer(unsigned('0' & INPUT1)) + unsigned('0' & INPUT2));
-	SUM <= RES(COMPONENT_SIZE - 1 to 0);
-	CARRY  <= RES(COMPONENT_SIZE);
+  RES <= std_logic_vector(unsigned('0' & INPUT1) + unsigned('0' & INPUT2));
+  SUM <= RES(COMPONENT_SIZE - 1 downto 0);
+  CARRY  <= RES(COMPONENT_SIZE);
 end architecture;
