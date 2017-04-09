e3337a9bd4d9cbcbe366806d6c66f18d9b368151
anvari1313 <ahmad.anv@gmail.com>
2017-04-09 11:49:38 +0430

    Implementing signed adder

--- alu/components/adder_component.vhd
+++ alu/components/adder_component.vhd
@@ -23,9 +23,48 @@ entity ADDER_COMPONENT is
 end entity;
 
 architecture ADDER_COMPONENT_ARCH of ADDER_COMPONENT is
-  signal RES    : std_logic_vector(COMPONENT_SIZE downto 0);
+	component FULL_ADDER is
+		port(
+			CIN			: in  std_logic;
+		    A			: in  std_logic;
+		    B			: in  std_logic;
+		    SUM       	: out std_logic;
+		    CARRY     	: out std_logic
+	      );
+	end component;
+	
+	signal CARRIES    : std_logic_vector(COMPONENT_SIZE downto 0);
+	signal LAST_CAR	  : std_logic;
+	
 begin
-  RES <= std_logic_vector(unsigned('0' & INPUT1) + unsigned('0' & INPUT2));
-  SUM <= RES(COMPONENT_SIZE - 1 downto 0);
-  CARRY  <= RES(COMPONENT_SIZE);
+	
+	
+	
+	CARRIES(0)	<= '0';
+	
+	ADDERS_CONNECTING:
+	for I in 0 to INPUT1'length - 1 generate
+		ADDER_X: FULL_ADDER
+			port map(
+				CIN   => CARRIES(I),
+				A     => INPUT1(I),
+				B     => INPUT2(I),
+				SUM   => SUM(I),
+				CARRY => CARRIES(I + 1)
+			);
+	end generate;
+--	
+--	ADDER_END :FULL_ADDER
+--		port map(
+--			CIN   => CARRIES(COMPONENT_SIZE - 1),
+--			A     => INPUT1(COMPONENT_SIZE - 1),
+--			B     => INPUT2(COMPONENT_SIZE - 1),
+--			SUM   => SUM(COMPONENT_SIZE - 1),
+--			CARRY => LAST_CAR
+--		); 
+--		
+	
+--	CARRY		<= (INPUT1(COMPONENT_SIZE - 1) xnor INPUT2(COMPONENT_SIZE - 1)) and LAST_CAR;
+
+	CARRY		<= CARRIES(COMPONENT_SIZE - 1) xor CARRIES(COMPONENT_SIZE);	
 end architecture;
