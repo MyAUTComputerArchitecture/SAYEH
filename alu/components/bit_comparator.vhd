223a58e1510da878eb9153493a01a53346c74ec2
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2017-04-10 16:21:21 +0430

    Change bit_comparator _component for comparation_component

--- alu/components/bit_comparator.vhd
+++ alu/components/bit_comparator.vhd
@@ -14,15 +14,22 @@ entity BIT_COMPARATOR_COMPONENT is
     port(
         INPUT1   : in std_logic;
         INPUT2   : in std_logic;
+        E_IN     : in std_logic;
+        L_IN     : in std_logic;
+        G_IN     : in std_logic;
         EQUAL    : out std_logic;
-        GREATER : out std_logic;
+        GREATER  : out std_logic;
         LOWER    : out std_logic
     );
 end entity;
 
-architecture BIT_COMPARATOR_ARCH of BIT_COMPARATOR is
+architecture BIT_COMPARATOR_ARCH of BIT_COMPARATOR_COMPONENT is
+    signal e, g, l : std_logic;
 begin
-    EQUAL   <= INPUT1 xnor INPUT2;
-    GREATER <= INPUT1 and (not INPUT2);
-    LOWER   <= (not INPUT1) and INPUT2;
+    e    <= INPUT1 xnor INPUT2;
+    g    <= INPUT1 and (not INPUT2);
+    l    <= (not INPUT1) and INPUT2;
+    GREATER <= (g or G_IN) and (not (L_IN or E_IN));
+    LOWER <= (l or L_IN) and (not (G_IN or E_IN));
+    EQUAL <= (e or E_IN) and (not (G_IN or L_IN));
 end architecture;
